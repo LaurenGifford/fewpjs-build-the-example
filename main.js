fddcc1a2425296f32d6cd66e3eecb356a7c073aa
LaurenGifford <lauren.gifford.17@gmail.com>
2021-02-06 19:28:23 -0500

    complete

--- main.js
+++ main.js
@@ -3,6 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorMessage = document.querySelector('#modal')
+
+window.addEventListener('load', hideError)
+
+document.addEventListener('click', function(e) {
+  if (e.target.className === 'like-glyph') {
+    console.log("heart clicked");
+    mimicServerCall()
+    .then(response => {
+       e.target.className = 'activated-heart'
+       e.target.innerText = FULL_HEART
+    }
+    )
+    .catch(error => {
+      console.log(error)
+      errorMessage.hidden = false
+      errorMessage.innerText = `${error}`
+      setTimeout(hideError, 5000)
+    })
+  }
+  else if (e.target.className === 'activated-heart') {
+    e.target.innerText = EMPTY_HEART
+    e.target.className = 'like-glyph'
+  }
+})
+
+
+function hideError(){
+  errorMessage.hidden = true
+}
+
+
+
 
 
 
